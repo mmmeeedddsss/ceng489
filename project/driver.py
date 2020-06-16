d9dceb6153822366d0561692ac13e0b2f5fd1fb1
Mert Tunc <mert.tunc@udemy.com>
2020-06-17 02:00:27 +0300

    Implemented classification_report support

--- project/driver.py
+++ project/driver.py
@@ -1,13 +1,14 @@
+import operator
 from sdn.data_loader import CustomSDNReader
-
 from random_forest import RandomForestWrapper
+from sklearn.metrics import classification_report
 
 data = CustomSDNReader.read(train_size=1000)
 
-print('Random forest with n labels accuracy',
-      RandomForestWrapper().fit_and_calculate_score(data['train']['X'],
-                                                    data['train']['y'],
-                                                    data['test']['X'],
-                                                    data['test']['y']))
+pred_y = RandomForestWrapper().fit_and_calculate_score(data['train']['X'],
+                                                       data['train']['y'],
+                                                       data['test']['X'])
 
-
+sorted_labels = sorted(data['y_encoding'].items(), key=operator.itemgetter(1))
+sorted_labels = [label[0] for label in sorted_labels]
+print(classification_report(data['test']['y'], pred_y, target_names=sorted_labels, digits=3))
--- project/random_forest.py
+++ project/random_forest.py
@@ -7,9 +7,8 @@ class RandomForestWrapper:
         print('Random Forest')
         self.classifier = RandomForestClassifier(n_estimators=50, n_jobs=8)
 
-    def fit_and_calculate_score(self, X, y, val_X, val_y):
+    def fit_and_calculate_score(self, X, y, test_X):
         print('Fitting to given X')
         self.classifier = self.classifier.fit(X, y)
-        print('Calculating score on test set')
-        return self.classifier.score(val_X, val_y)
+        return self.classifier.predict(test_X)
 
