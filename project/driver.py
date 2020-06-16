0a9d297727217d4ab230a386d0b4d4974c7c5bb5
Mert Tunc <mert.tunc@udemy.com>
2020-06-17 01:45:30 +0300

    Fixed mini bug

--- project/driver.py
+++ project/driver.py
@@ -7,7 +7,7 @@ data = CustomSDNReader.read(train_size=1000)
 print('Random forest with n labels accuracy',
       RandomForestWrapper().fit_and_calculate_score(data['train']['X'],
                                                     data['train']['y'],
-                                                    data['validation']['X'],
-                                                    data['validation']['y']))
+                                                    data['test']['X'],
+                                                    data['test']['y']))
 
 
